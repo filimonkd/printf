4be05851f22577efed4629f2707a15548d41c7c0
filimonkd <filimonkd@gmail.com>
2022-11-23 10:27:34 -0800

    print char and string

---
+++ main.h
@@ -0,0 +1,50 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * struct format - a struct of format
+ * @l: format checker
+ * @ptr:  a pointer function
+ */
+
+typedef struct format
+{
+	char l;
+	int (*ptr)(va_list);
+} Data;
+
+
+/**
+ * struct sign - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag fpr the '+' character
+ * @space:flag for the ' 'character
+ * @hash: flag for the '#' character
+ */
+
+typedef struct sign
+{
+	int plus;
+	int space;
+	int hash;
+} sign_t;
+
+/* format checker*/
+int print_format(const char *format, va_list valist);
+int formatchecker(const char *str, va_list valist, int *j);
+
+/*print the output to stdout*/
+int print_out(char c);
+int buffer(char c);
+
+/*print anything*/
+int _printf(const char *format, ...);
+
+/*print a character and string*/
+int print_char(va_list valist);
+int print_string(va_list valist);
+int _puts(char *str);
+int print_rev(va_list valist);
