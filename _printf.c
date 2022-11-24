929b56ba51685488f767eb6b36c146f2e3993c4b
filimonkd <filimonkd@gmail.com>
2022-11-24 11:59:35 -0800

    update printf

--- _printf.c
+++ _printf.c
@@ -43,7 +43,14 @@ int formatchecker(const char *str, va_list valist, int *j)
 	int formats;
 
 	Data checker[] = {{'c', print_char},
-			  {'s', print_string}};
+			  {'s', print_string},
+			  {'d', print_int},
+			  {'i', print_int},
+			  {'b', print_binary},
+			  {'u', print_unsigned},
+			  {'o', print_octal},
+			  {'x', print_hex},
+			  {'X', print_hex_big};
 	*j = *j + 1;
 	if (str[*j] == '\0')
 	{
